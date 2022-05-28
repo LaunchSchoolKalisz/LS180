ff94068b3668dee02ff09c8f3ab5563ee6456433
mskalisz <mskalisz3@gmail.com>
2022-05-28 08:50:58 -0400

    Fix spacing

--- database_book/ls_burger.sql
+++ database_book/ls_burger.sql
@@ -6,7 +6,6 @@ CREATE TABLE orders (
     drink text
 );
 
-
 INSERT INTO orders VALUES (1, 'Todd Perez', 'LS Burger', 'Fries', 'Lemonade');
 INSERT INTO orders VALUES (2, 'Florence Jordan', 'LS Cheeseburger', 'Fries', 'Chocolate Shake');
 INSERT INTO orders VALUES (3, 'Robin Barnes', 'LS Burger', 'Onion Rings', 'Vanilla Shake');
